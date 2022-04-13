74286259f20772cd94c0ecacf6f3c4bf7fd0e5da
Annew <60906912+qing-2@users.noreply.github.com>
2022-04-13 09:39:23 +0800

    Update top_tb.v

--- top_tb.v
+++ top_tb.v
@@ -26,36 +26,33 @@ module top_tb;
     wire clk;
     wire [31:0]pc;
     wire [31:0]inst;
-//    wire [31:0]addr;
-//    wire [31:0]rdata;
-//    wire [31:0]wdata;
     wire IM_R;
     wire DM_CS;
     wire DM_R;
     wire DM_W;
-    
-//    wire [31:0]rf_rdata1;
-//    wire [31:0]rf_rdata2;
-//    wire [31:0]rf_wdata;
+
     wire [31:0]alu_r;
-//    wire [2:0]alu_c;
-//    wire [31:0]m1_out;    
-//    wire [31:0]m2_out;    
-//    wire [31:0]m3_out;
-//    wire [31:0]m4_out;
-//    wire [31:0]m5_out;    
-//    wire [4:0]m6_out;
-//    wire z;
+
     top uut(clk_in,reset,clk,pc,inst,addr,rdata,wdata,IM_R,DM_CS,DM_R,DM_W,alu_r);
-    initial
-        begin
+
+    // 不同的initial块是并行的，块内部顺序执行
+    initial 
+    begin
         clk_in = 0;
+        forever #100 clk_in = ~clk_in;
+    end
+
+    initial
+    begin
         reset = 1;
-        #100;
-        reset=0;
-        forever
-            #100
-            clk_in = ~clk_in;
-        end
+        #50 reset=0;
+        #2000 $stop;
+    end
+        
+    initial // 用iverilog+gtkwave仿真时用到的
+    begin            
+        $dumpfile("wave.vcd");        //生成的vcd文件名称
+        $dumpvars(0, top_tb);    //tb模块名称
+    end
 
 endmodule
